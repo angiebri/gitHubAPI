6f10e17efac054067836dd2b980723a42fc8177f
Angelina <ani-bri@ya.ru>
2023-08-16 19:11:34 +0300

    Axios, главная страница

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,25 @@ import './App.less'
 import styled from 'styled-components'
 import { useDispatch, useSelector } from 'react-redux'
 import { setCount } from '../reducers/reposReducer'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 const App = () => {
     const dispatch = useDispatch()
-    const onCountClick = () => dispatch(setCount(4))
-    const count = useSelector((state) => state.repos.count)
 
-    const Button = styled.button`
+    /*const Button = styled.button`
         background: transparent;
         border-radius: 3px;
         border: 2px solid #bf4f74;
         color: #bf4f74;
         margin: 0 1em;
         padding: 0.25em 1em;
-    `
+    `*/
+
     return (
-        <div className='app'>
-            <Button variant='contained' onClick={() => onCountClick()}>
-                COUNT
-            </Button>
-            <div>{count}</div>
-        </div>
+        <BrowserRouter>
+            <div></div>
+            <Route path='/' Component={Main}></Route>
+        </BrowserRouter>
     )
 }
 
